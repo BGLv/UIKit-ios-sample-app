2ba49fddb8920a6b2aee2df4d2f48408403514ac
BogdanGuk <bogdanguk@gmail.com>
2025-08-16 13:35:51 +0300

    Initial Commit

---
+++ MyMarketTests/MyMarketTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyMarketTests.swift
+//  MyMarketTests
+//
+//  Created by Bohdan Huk on 16.08.2025.
+//
+
+import Testing
+@testable import MyMarket
+
+struct MyMarketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
